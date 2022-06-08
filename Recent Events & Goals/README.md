90866c0c7739d1cb396616442c55b3a7730a8017
kokomanu31 <emanuel.kokomanu31@gmail.com>
2022-06-08 17:41:54 -0400

    Added rest of events and goals
    - allow to enable recent cheers
    - allow to enable recent tips
    - allow to enable recent raids
    - allow to enable follower goal
    - allow to enable subscriber goal
    - allow to enable tip goal
    - Choose between Total, Goal, Session, Weekly and Monthly goal counts

--- Recent Events & Goals/README.md
+++ Recent Events & Goals/README.md
@@ -1,14 +1,14 @@
 # Recent events & goals widget
 
-All in one small and compact widget allowing to display not only recent events but also multiple different goals.
+All-in-one, small and compact widget displaying recent events and goals.
 
-Every event and goal can be turned on/off for full customization.
+Each event and goal can be turned on/off and is fully customizable.
 
-🚧 *More to come, under construction...* 🚧
+🚧 *More information to come, under construction...* 🚧
 
 ### Project objective
 
-Have a all-in-one widget able to show:
+Have an all-in-one widget able to show:
 - Recent events
     - Followers
     - Subscribers
@@ -20,6 +20,87 @@ Have a all-in-one widget able to show:
     - Subscriber
     - Cheer
     - Tip
+
+### Levels of customization
+
+#### Global Settings
+- Change the number of displayed names for all events
+- Change the duration of every event
+- Enable/disable specific events
+#### Recent Followers
+- Change the title of the event
+- Change the template used for each username
+- Enable/disable scrolling
+- Change the scrolling speed
+- _Use `{name}` to display the username_
+
+#### Recent Subscribers
+- Change the title of the event
+- Change the template used for each username
+- Enable/disable scrolling
+- Change the scrolling speed
+- _Use `{name}` to display the username_
+- _Use `{amount}` to display the number of months resubbed_
+
+#### Recent Cheers
+- Change the title of the event
+- Change the template used for each username
+- Enable/disable scrolling
+- Change the scrolling speed
+- _Use `{name}` to display the username_
+- _Use `{amount}` to display the number of bits cheered_
+
+#### Recent Tips
+- Change the title of the event
+- Change the template used for each username
+- Enable/disable scrolling
+- Change the scrolling speed
+- _Use `{name}` to display the username_
+- _Use `{amount}` to display the amount tipped_
+
+#### Recent Raids
+- Change the title of the event
+- Change the template used for each username
+- Enable/disable scrolling
+- Change the scrolling speed
+- _Use `{name}` to display the username_
+- _Use `{amount}` to display the number of viewers raided with_
+
+#### Follower Goal
+- Change the title of the event
+- Change the template used
+- Change the used count information:
+    - `Total`: displays the total number of followers
+    - `Goal`: uses the number of followers in the goals section of session data
+    - `Session`: uses the number of followers during the current streaming session
+    - `Weekly`: uses the number of followers during the current week
+    - `Monthly`: uses the number of followers during the current month
+- _Use `{count}` to display the follower count_
+
+#### Subscriber Goal
+- Change the title of the event
+- Change the template used
+- Change the used count information:
+    - `Total`: displays the total number of subscribers
+    - `Goal`: uses the number of subscribers in the goals section of session data
+    - `Session`: uses the number of subscribers during the current streaming session
+    - `Weekly`: uses the number of subscribers during the current week
+    - `Monthly`: uses the number of subscribers during the current month
+- _Use `{count}` to display the subscriber count_
+
+#### Cheer Goal
+🚧 *Not yet implemented...* 🚧
+
+#### Tip Goal
+- Change the title of the event
+- Change the template used
+- Change the used count information:
+    - `Total`: displays the total number of subscribers
+    - `Goal`: uses the total amount received in the goals section of session data
+    - `Session`: uses the total amount received during the current streaming session
+    - `Weekly`: uses the total amount received during the current week
+    - `Monthly`: uses the total amount received during the current month
+- _Use `{count}` to display the tip count_
 
 ### Watch the programming LIVE
 I work on this project live on [Twitch](https://www.twitch.tv/kokomanu_31) so if you ever want to see it in action or learn more about it, feel free to drop by!
--- Recent Events & Goals/widget.js
+++ Recent Events & Goals/widget.js
@@ -1,8 +1,8 @@
-let fieldData, sessionData, nbPerEvent = {eventsLimit}, events = [], currentEvent = 0;
+let nbPerEvent = {eventsLimit}, events = [], currentEvent = 0;
 
 function fillFollowers(followerData) {
   followers = followerData.slice(0, nbPerEvent);
-  followerTemplate = `{followerEventText}`;
+  followerTemplate = '{followerEventText}';
   text = '';
   for (var i = 0; i < followers.length; i++) {
   	if (i < followers.length - 1)
@@ -20,7 +20,7 @@ function fillFollowers(followerData) {
 
 function fillSubscribers(subscriberData) {
   subscribers = subscriberData.slice(0, nbPerEvent);
-  subscriberTemplate = `{subscriberEventText}`;
+  subscriberTemplate = '{subscriberEventText}';
   text = '';
   for (var i = 0; i < subscribers.length; i++) {
   	if (i < subscribers.length - 1)
@@ -36,8 +36,80 @@ function fillSubscribers(subscriberData) {
   }
 }
 
+function fillCheers(cheerData) {
+  cheers = cheerData.slice(0, nbPerEvent);
+  cheerTemplate = '{cheerEventText}';
+  text = '';
+  for (var i = 0; i < cheers.length; i++) {
+  	if (i < cheers.length - 1)
+      text = text.concat(cheerTemplate.replace('{name}', cheers[i].name).replace('{amount}', cheers[i].amount)).concat('   ');
+    else
+      text = text.concat(cheerTemplate.replace('{name}', cheers[i].name).replace('{amount}', cheers[i].amount).replace(',', '')).concat('   ');
+  }
+  $('#recent-cheers-text')[0].textContent = text;
+  if ({cheerEnableScroll} === true) {
+    $('#recent-cheers-text').addClass('scroll');
+  } else {
+    $('#recent-cheers-text').removeClass('scroll');
+  }
+}
+
+function fillTips(tipData) {
+  tips = tipData.slice(0, nbPerEvent);
+  tipTemplate = '{tipEventText}';
+  text = '';
+  for (var i = 0; i < tips.length; i++) {
+  	if (i < tips.length - 1)
+      text = text.concat(tipTemplate.replace('{name}', tips[i].name).replace('{amount}', tips[i].amount)).concat('   ');
+    else
+      text = text.concat(tipTemplate.replace('{name}', tips[i].name).replace('{amount}', tips[i].amount).replace(',', '')).concat('   ');
+  }
+  $('#recent-tips-text')[0].textContent = text;
+  if ({tipEnableScroll} === true) {
+    $('#recent-tips-text').addClass('scroll');
+  } else {
+    $('#recent-tips-text').removeClass('scroll');
+  }
+}
+
+function fillRaids(raidData) {
+  raids = raidData.slice(0, nbPerEvent);
+  raidTemplate = '{raidEventText}';
+  text = '';
+  for (var i = 0; i < raids.length; i++) {
+  	if (i < raids.length - 1)
+      text = text.concat(raidTemplate.replace('{name}', raids[i].name).replace('{amount}', raids[i].amount)).concat('   ');
+    else
+      text = text.concat(raidTemplate.replace('{name}', raids[i].name).replace('{amount}', raids[i].amount).replace(',', '')).concat('   ');
+  }
+  $('#recent-raids-text')[0].textContent = text;
+  if ({raidEnableScroll} === true) {
+    $('#recent-raids-text').addClass('scroll');
+  } else {
+    $('#recent-raids-text').removeClass('scroll');
+  }
+}
+
+function fillFollowerGoal(followerCount) {
+  followerGoalTemplate = '{followerGoalText}';
+  text = followerGoalTemplate.replace('{count}', followerCount);
+  $('#follower-goal-text')[0].textContent = text;
+}
+
+function fillSubscriberGoal(subscriberCount) {
+  subscriberGoalTemplate = '{subscriberGoalText}';
+  text = subscriberGoalTemplate.replace('{count}', subscriberCount);
+  $('#subscriber-goal-text')[0].textContent = text;
+}
+
+function fillTipGoal(tipCount) {
+  tipGoalTemplate = '{tipGoalText}';
+  text = tipGoalTemplate.replace('{count}', tipCount);
+  $('#tip-goal-text')[0].textContent = text;
+}
+
 function loop() {
-  if (events.length === 0)
+  if (events.length === 1)
     return;
   
   nextEvent = 0;
@@ -45,47 +117,190 @@ function loop() {
     nextEvent = currentEvent + 1;
   nextEventName = events[nextEvent];
   
-  if (events[currentEvent] === "recent-followers") {
+  if (events[currentEvent] === "recent-followers")
     fadeOutElement = $("#recent-followers");
-  } else if (nextEventName === "recent-followers") {
+  else if (nextEventName === "recent-followers")
     fadeInElement = $("#recent-followers");
-  }
   
-  if (events[currentEvent] === "recent-subscribers") {
+  if (events[currentEvent] === "recent-subscribers")
     fadeOutElement = $("#recent-subscribers");
-  } else if (nextEventName === "recent-subscribers") {
+  else if (nextEventName === "recent-subscribers")
     fadeInElement = $("#recent-subscribers");
-  }
+    
+  if (events[currentEvent] === "recent-cheers")
+    fadeOutElement = $("#recent-cheers");
+  else if (nextEventName === "recent-cheers")
+    fadeInElement = $("#recent-cheers");
+  
+  if (events[currentEvent] === "recent-tips")
+    fadeOutElement = $("#recent-tips");
+  else if (nextEventName === "recent-tips")
+    fadeInElement = $("#recent-tips");
+  
+  if (events[currentEvent] === "recent-raids")
+    fadeOutElement = $("#recent-raids");
+  else if (nextEventName === "recent-raids")
+    fadeInElement = $("#recent-raids");
+  
+  if (events[currentEvent] === "follower-goal")
+    fadeOutElement = $("#follower-goal");
+  else if (nextEventName === "follower-goal")
+    fadeInElement = $("#follower-goal");
+  
+  if (events[currentEvent] === "subscriber-goal")
+    fadeOutElement = $("#subscriber-goal");
+  else if (nextEventName === "subscriber-goal")
+    fadeInElement = $("#subscriber-goal");
+  
+  if (events[currentEvent] === "tip-goal")
+    fadeOutElement = $("#tip-goal");
+  else if (nextEventName === "tip-goal")
+    fadeInElement = $("#tip-goal");
   
   currentEvent = nextEvent;
-  console.log('switching...');
   
   fadeOutElement.fadeOut('slow', function() {
     fadeInElement.fadeIn('slow');
   });
 }
 
-window.addEventListener('onWidgetLoad', (obj) => {
-  console.log('sessionData has been created!', obj.detail.session);
-    
+window.addEventListener('onWidgetLoad', (obj) => {    
   if ({enableRecentFollowers}) {
-  	events.push("recent-followers");  
+  	events.push("recent-followers");
+    $("#recent-followers").removeClass('hidden');
   	fillFollowers(obj.detail.session.data["follower-recent"]);
   }
   
   if ({enableRecentSubs}) {
     events.push("recent-subscribers");
+    $("#recent-subscribers").removeClass('hidden');
     fillSubscribers(obj.detail.session.data["subscriber-recent"]);
   }
   
-  setInterval(loop, 10_000);
+  if ({enableRecentCheers}) {
+  	events.push("recent-cheers");
+    $("#recent-cheers").removeClass('hidden');
+    fillCheers(obj.detail.session.data["cheer-recent"]);
+  }
+  
+  if ({enableRecentTips}) {
+  	events.push("recent-tips");
+    $("#recent-tips").removeClass('hidden');
+    fillTips(obj.detail.session.data["tip-recent"]);
+  }
+  
+  if ({enableRecentRaids}) {
+  	events.push("recent-raids");
+    $("#recent-raids").removeClass('hidden');
+    fillRaids(obj.detail.session.data["raid-recent"]);
+  }
+  
+  if ({enableFollowerGoal}) {
+  	events.push("follower-goal");
+    $("#follower-goal").removeClass('hidden');
+    followerCountType = '{followerTypeDrowpdown}';
+    if (followerCountType === "total")
+      fillFollowerGoal(obj.detail.session.data["follower-total"].count);
+    else if (followerCountType === "goal")
+      fillFollowerGoal(obj.detail.session.data["follower-goal"].amount);
+    else if (followerCountType === "session")
+      fillFollowerGoal(obj.detail.session.data["follower-session"].count);
+    else if (followerCountType === "weekly")
+      fillFollowerGoal(obj.detail.session.data["follower-week"].count);
+    else
+      fillFollowerGoal(obj.detail.session.data["follower-month"].count);
+  }
+  
+  if ({enableSubscriberGoal}) {
+  	events.push("subscriber-goal");
+    $("#subscriber-goal").removeClass('hidden');
+    subscriberCountType = '{subscriberTypeDrowpdown}';
+    if (subscriberCountType === "total")
+      fillSubscriberGoal(obj.detail.session.data["subscriber-total"].count);
+    else if (subscriberCountType === "goal")
+      fillSubscriberGoal(obj.detail.session.data["subscriber-goal"].amount);
+    else if (subscriberCountType === "session")
+      fillSubscriberGoal(obj.detail.session.data["subscriber-session"].count);
+    else if (subscriberCountType === "weekly")
+      fillSubscriberGoal(obj.detail.session.data["subscriber-week"].count);
+    else
+      fillSubscriberGoal(obj.detail.session.data["subscriber-month"].count);
+  }
+  
+  if ({enableTipGoal}) {
+  	events.push("tip-goal");
+    $("#tip-goal").removeClass('hidden');
+    tipCountType = '{tipTypeDrowpdown}';
+    if (tipCountType === "total")
+      fillTipGoal(obj.detail.session.data["tip-total"].amount);
+    else if (tipCountType === "goal")
+      fillTipGoal(obj.detail.session.data["tip-goal"].amount);
+    else if (tipCountType === "session")
+      fillTipGoal(obj.detail.session.data["tip-session"].amount);
+    else if (tipCountType === "weekly")
+      fillTipGoal(obj.detail.session.data["tip-week"].amount);
+    else
+      fillTipGoal(obj.detail.session.data["tip-month"].amount);
+  }
+  
+  setInterval(loop, {eventsTime} * 1000);
 });
 
 window.addEventListener('onSessionUpdate', (obj) => {
-  console.log('sessionData has been updated', obj.detail.session);
   if ({enableRecentFollowers})
   	fillFollowers(obj.detail.session.data["follower-recent"]);
-  
+    
   if ({enableRecentSubs})
     fillSubscribers(obj.detail.session.data["subscriber-recent"]);
+  
+  if ({enableRecentCheers})
+    fillCheers(obj.detail.session.data["cheers-recent"]);
+  
+  if ({enableRecentTips})
+    fillTips(obj.detail.session.data["tip-recent"]);
+  
+  if ({enableRecentRaids})
+    fillRaids(obj.detail.session.data["raid-recent"]);
+  
+  if ({enableFollowerGoal}) {
+    followerCountType = '{followerTypeDrowpdown}';
+    if (followerCountType === "total")
+      fillFollowerGoal(obj.detail.session.data["follower-total"].count);
+    else if (followerCountType === "goal")
+      fillFollowerGoal(obj.detail.session.data["follower-goal"].amount);
+    else if (followerCountType === "session")
+      fillFollowerGoal(obj.detail.session.data["follower-session"].count);
+    else if (followerCountType === "weekly")
+      fillFollowerGoal(obj.detail.session.data["follower-week"].count);
+    else
+      fillFollowerGoal(obj.detail.session.data["follower-month"].count);
+  }
+  
+  if ({enableSubscriberGoal}) {
+    subscriberCountType = '{subscriberTypeDrowpdown}';
+    if (subscriberCountType === "total")
+      fillSubscriberGoal(obj.detail.session.data["subscriber-total"].count);
+    else if (subscriberCountType === "goal")
+      fillSubscriberGoal(obj.detail.session.data["subscriber-goal"].amount);
+    else if (subscriberCountType === "session")
+      fillSubscriberGoal(obj.detail.session.data["subscriber-session"].count);
+    else if (subscriberCountType === "weekly")
+      fillSubscriberGoal(obj.detail.session.data["subscriber-week"].count);
+    else
+      fillSubscriberGoal(obj.detail.session.data["subscriber-month"].count);
+  }
+  
+  if ({enableTipGoal}) {
+    tipCountType = '{tipTypeDrowpdown}';
+    if (tipCountType === "total")
+      fillTipGoal(obj.detail.session.data["tip-total"].amount);
+    else if (tipCountType === "goal")
+      fillTipGoal(obj.detail.session.data["tip-goal"].amount);
+    else if (tipCountType === "session")
+      fillTipGoal(obj.detail.session.data["tip-session"].amount);
+    else if (tipCountType === "weekly")
+      fillTipGoal(obj.detail.session.data["tip-week"].amount);
+    else
+      fillTipGoal(obj.detail.session.data["tip-month"].amount);
+  }
 });
